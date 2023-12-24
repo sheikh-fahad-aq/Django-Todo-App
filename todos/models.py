2ce69e640b882e10effb673843925e5d6ba6d41e
sheikh-fahad-aq <fahadahmedqureshi.93@gmail.com>
2023-12-24 12:59:49 +0500

    code has been started for todo app

---
+++ todos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    completed = models.BooleanField(default='False')
+
+    def __str__(self):
+        return f'Todo with title: %s' % self.title
